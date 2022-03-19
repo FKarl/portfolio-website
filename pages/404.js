2fa610aa039120dcb50b9dab10a19c4d6c317872
Fabian Karl <fabian.karl@uni-ulm.de>
2022-03-19 20:04:35 +0100

    fix: smaller text

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ export default function PageNotFound() {
     return (
         <div className="w-screen h-screen bg-404-v lg:bg-404 bg-center bg-cover">
             <div className="flex flex-col items-center justify-center top-1/2 left-1/2 absolute -translate-x-1/2 -translate-y-1/2 space-y-8">
-                <h1 className="whitespace-nowrap text-2xl tracking-tight font-bold sm:text-4xl md:text-5xl text-dark-headline-500">
+                <h1 className="whitespace-nowrap text-xl tracking-tight font-bold sm:text-4xl md:text-5xl text-dark-headline-500">
                     404 - This page could not be found
                 </h1>
                 <Link href="/">
