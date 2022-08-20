ae29c2ced7dae0a0638a32dd5e27adc45600c3c6
Fabian Karl <fabian.karl@uni-ulm.de>
2022-08-20 17:05:17 +0200

    improvement: remove slide animation

--- components/sections/Section.js
+++ components/sections/Section.js
@@ -1,41 +1,14 @@
 import {CgArrowLongRightC, CgArrowLongDownC} from "react-icons/cg"
-import {motion, useAnimation} from "framer-motion"
-import {useInView} from "react-intersection-observer";
-import {useEffect} from "react";
 import React from "react";
 
 export default function Section({children, side, title, id, className}) {
     if (!((side === "l") || (side === "r"))) side = "l";
 
-    const {ref, inView} = useInView({
-        rootMargin: "-25%"
-    });
-    const animation = useAnimation();
-
-    useEffect(() => {
-        if (inView) {
-            animation.start(
-                {
-                    x: 0,
-                    transition: {
-                        type: "spring",
-                        duration: 1.5,
-                        delay: 0,
-                        bounce: 0.3,
-                    }
-                }
-            )
-        }
-    }, [inView, animation]);
 
     return (
         <section className={className}>
-            <div ref={ref} id={id} className="md:-mt-32 md:pt-32 lg:-mt-48 lg:pt-48 overflow-hidden">
-                <motion.div
-                    initial={{
-                        x: side === "l" ? "-100vw" : "100vw",
-                    }}
-                    animate={animation}
+            <div id={id} className="md:-mt-32 md:pt-32 lg:-mt-48 lg:pt-48 overflow-hidden">
+                <div
                     className={`pl-4 pr-4 md:pl-0 md:pr-0 mt-3 md:mt-5 pb-8 w-full lg:max-w-7xl mx-auto flex flex-col items-start space-y-6 md:space-y-0 md:space-x-6 ${side === "l" ? "md:flex-row md:[padding-right:4.25rem]" : "md:flex-row-reverse space-x-reverse md:[padding-left:4.25rem]"}`}
                 >
 
@@ -54,7 +27,7 @@ export default function Section({children, side, title, id, className}) {
                     <div className="flex-grow w-full">
                         {children}
                     </div>
-                </motion.div>
+                </div>
             </div>
         </section>
     )
